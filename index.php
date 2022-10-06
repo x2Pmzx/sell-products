679c48e9e710e46c3f2904986b63d9e7ff971c43
x2Pmzx <115133941+x2Pmzx@users.noreply.github.com>
2022-10-06 12:21:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,216 @@
+﻿<?php
+session_start();
+error_reporting(0);
+include("plugin/hyper_api.php");
+require "plugin/directory.php";
+
+  $web_sql = "SELECT * FROM web_config WHERE con_id = 1";
+  $web_con = $hyper->connect->query($web_sql)->fetch_array();
+
+  $webname = $web_con['name'];
+  $webfacebook = $web_con['facebook'];
+  $webdetail = $web_con['detail'];
+  $webimage = $web_con['image'];
+  $webopen = $web_con['opened'];
+
+if(isset($_SESSION['USER_SID'])){
+
+  $loged = 1;
+  $sid = $_SESSION['USER_SID'];
+  $var = "SELECT * FROM accounts WHERE sid = '".$sid."' ";
+  $data_user = $hyper->connect->query($var)->fetch_array();
+
+  $username = $data_user['username'];
+  $points = number_format($data_user['points'],0);
+  $email = $data_user['email'];
+  $role =$data_user['role'];
+  $sid =$data_user['sid'];
+  $ac_id =$data_user['ac_id'];
+
+  setcookie('USER_SID', $sid, time()+1*24*60*60);
+
+}else{
+  $loged = 0;
+}
+
+if(empty($_GET['thispage'])){
+  $_GET['thispage'] = 'home';
+}
+
+$page = $_GET['thispage'];
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title><?= strtoupper($webname); ?></title>
+
+<link rel="shortcut icon" href="<?= $url ?>assets/img/<?= $webimage; ?>" type="image/png">
+<link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
+<link href="<?= $url ?>assets/css/bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="<?= $url ?>assets/css/jquery.dataTables.min.css">
+<link rel="stylesheet" type="text/css" href="<?= $url ?>assets/css/dataTables.bootstrap4.min.css">
+<link href="<?= $url ?>assets/css/animate.css" rel="stylesheet">
+<script src="<?= $url ?>assets/js/sweetalert.min.js"></script>
+<link href="<?= $url ?>assets/css/hyper.css" rel="stylesheet">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-VhBcF/php0Z/P5ZxlxaEx1GwqTQVIBu4G4giRWxTKOCjTxsPFETUDdVL5B6vYvOt" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<script type="text/javascript" src="<?= $url ?>assets/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+<script>
+  $(document).ready(function() {
+      $('[data-toggle="tooltip"]').tooltip();
+      $('#datatable').DataTable();
+  });
+  </script>
+  <style>
+  .swal-modal .swal-text {
+    text-align: center;
+}
+  </style>
+</head>
+<body>
+
+
+    <!-- User Navbar -->
+    <?php
+      if($loged == 1){
+        include('page/user/user_nav.php');
+      }else{
+        include('page/nav.php');
+      }
+    ?>
+    <!-- End User Navbar -->
+
+    <!-- Container Start -->
+    <div class="container" style="padding-top: 110px;">
+
+    <?php
+      if($loged == 1){
+
+        if($page == 'home'){
+          include('page/welcome.php');
+        }elseif($page == 'profile'){
+          include('page/user/profile.php'); 
+        }elseif($page == 'history'){
+          include('page/user/myhistory.php'); 
+        }elseif($page == 'topup'){
+          include('page/user/pay.php'); 
+        }elseif($page == 'shop'){
+          include('page/user/shop.php'); 
+        }elseif($page == 'item'){
+          include('page/user/iteminfo.php'); 
+        }elseif($page == 'logout'){
+          include('page/logout.php'); 
+        }elseif($page == 'adminsys'){
+          if($data_user['role'] == '779'){
+            include('page/admin/dash.php');
+          }else{
+            $page = 'home';
+            include('page/welcome.php'); 
+          }
+        }elseif($page == 'gametype'){
+          if($data_user['role'] == '779'){
+            include('page/admin/game_edit.php');
+          }else{
+            $page = 'home';
+            include('page/welcome.php'); 
+          }
+        }elseif($page == 'gameselect'){
+          if($data_user['role'] == '779'){
+            include('page/admin/game_item/game_select.php');
+          }else{
+            $page = 'home';
+            include('page/welcome.php'); 
+          }
+        }elseif($page == 'editgame'){
+          if($data_user['role'] == '779'){
+            include('page/admin/game_item/menu_game_edit.php');
+          }else{
+            $page = 'home';
+            include('page/welcome.php'); 
+          }
+        }elseif($page == 'gamecard'){
+          if($data_user['role'] == '779'){
+            include('page/admin/game_item/edit_game_card.php');
+          }else{
+            $page = 'home';
+            include('page/welcome.php'); 
+          }
+        }elseif($page == 'gamedata'){
+          if($data_user['role'] == '779'){
+            include('page/admin/game_item/game_data.php');
+          }else{
+            $page = 'home';
+            include('page/welcome.php'); 
+          }
+        }elseif($page == 'dataowner'){
+          if($data_user['role'] == '779'){
+            include('page/admin/data_owner.php');
+          }else{
+            $page = 'home';
+            include('page/welcome.php'); 
+          }
+        }elseif($page == 'datauser'){
+          if($data_user['role'] == '779'){
+            include('page/admin/data_user.php');
+          }else{
+            $page = 'home';
+            include('page/welcome.php'); 
+          }
+        }elseif($page == 'datapay'){
+          if($data_user['role'] == '779'){
+            include('page/admin/history_pay.php');
+          }else{
+            $page = 'home';
+            include('page/welcome.php'); 
+          }
+        }elseif($page == 'websetting'){
+          if($data_user['role'] == '779'){
+            include('page/admin/web_config.php');
+          }else{
+            $page = 'home';
+            include('page/welcome.php'); 
+          }
+        }elseif($page == 'test'){
+          include('page/user/profile.php'); 
+        }
+        
+        
+
+        else{
+          $page = 'home';
+          include('page/welcome.php'); 
+        }
+
+      }else{
+
+        if($page == 'login'){
+          include('page/sign-in.php');
+        }elseif($page == 'register'){
+          include('page/sign-up.php'); 
+        }elseif($page == 'resetpassword'){
+          include('page/resetpassword.php'); 
+        }else{
+          $page = 'home';
+          include('page/welcome.php'); 
+        }
+        
+      }
+    ?>
+
+    <!-- Footer -->
+      <small class="pb-3 d-block my-auto footer-copyright text-secondary text-center py-4 w-100">©  Copyright 2021 Website By <a href="https://www.facebook.com/pagehyperstudio" target="_blank">Hyper Studio</a> All Rights Reserved.</small>
+    <!-- End Footer -->
+    </div>
+    <!-- Container End -->
+
+
+</body>
+</html>
---
+++ shop.sql
@@ -0,0 +1,296 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Generation Time: 25 ก.พ. 2021 เมื่อ 07:58 AM
+-- เวอร์ชันของเซิร์ฟเวอร์: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gameid_shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- โครงสร้างตาราง `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `ac_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(512) NOT NULL,
+  `salt` varchar(100) NOT NULL,
+  `email` text NOT NULL,
+  `points` float NOT NULL DEFAULT 0,
+  `sid` varchar(256) NOT NULL,
+  `role` int(3) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- dump ตาราง `accounts`
+--
+
+INSERT INTO `accounts` (`ac_id`, `username`, `password`, `salt`, `email`, `points`, `sid`, `role`) VALUES
+(33, 'suwisit', '7437caa5cdeb674ac24404ed8811a6d7', '08c3b702f03e71e9', 'suwisit7700@gmail.com', 99945, 'YWI2NTE2NTRkMDE0MDY1NDIwc3V3aXNpdA==', 779),
+
+-- --------------------------------------------------------
+
+--
+-- โครงสร้างตาราง `card_image`
+--
+
+CREATE TABLE `card_image` (
+  `image_id` int(11) NOT NULL,
+  `card_id` int(11) NOT NULL,
+  `image_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- โครงสร้างตาราง `data_selled`
+--
+
+CREATE TABLE `data_selled` (
+  `selled_id` int(11) NOT NULL,
+  `data_id` int(11) NOT NULL,
+  `ac_id` int(11) NOT NULL,
+  `selled_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- โครงสร้างตาราง `game_card`
+--
+
+CREATE TABLE `game_card` (
+  `card_id` int(11) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `card_title` text NOT NULL,
+  `card_price` double NOT NULL,
+  `card_detail` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- โครงสร้างตาราง `game_data`
+--
+
+CREATE TABLE `game_data` (
+  `data_id` int(11) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `card_id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `detail` text NOT NULL,
+  `selled` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- โครงสร้างตาราง `game_type`
+--
+
+CREATE TABLE `game_type` (
+  `game_id` int(11) NOT NULL,
+  `game_name` text NOT NULL,
+  `game_image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- โครงสร้างตาราง `history_pay`
+--
+
+CREATE TABLE `history_pay` (
+  `pay_id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `link` text NOT NULL,
+  `amount` float NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- dump ตาราง `history_pay`
+--
+
+INSERT INTO `history_pay` (`pay_id`, `username`, `link`, `amount`, `date`) VALUES
+(3, 'hyperstudio', 'y7gMecjIKprVJemngf', 10, '2021-02-25 11:55:01');
+
+-- --------------------------------------------------------
+
+--
+-- โครงสร้างตาราง `image_slide`
+--
+
+CREATE TABLE `image_slide` (
+  `slide_id` int(11) NOT NULL,
+  `image_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- dump ตาราง `image_slide`
+--
+
+INSERT INTO `image_slide` (`slide_id`, `image_name`) VALUES
+(1, 'bannerani.png');
+
+-- --------------------------------------------------------
+
+--
+-- โครงสร้างตาราง `web_config`
+--
+
+CREATE TABLE `web_config` (
+  `con_id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `facebook` text NOT NULL,
+  `detail` text NOT NULL,
+  `image` text NOT NULL,
+  `opened` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- dump ตาราง `web_config`
+--
+
+INSERT INTO `web_config` (`con_id`, `name`, `facebook`, `detail`, `image`, `opened`) VALUES
+(1, 'hyperstudio', 'pagehyperstudio', 'Hello hyperstudio!', '682f025c739fef2c81303b618f811df7_logo.png', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`ac_id`);
+
+--
+-- Indexes for table `card_image`
+--
+ALTER TABLE `card_image`
+  ADD PRIMARY KEY (`image_id`);
+
+--
+-- Indexes for table `data_selled`
+--
+ALTER TABLE `data_selled`
+  ADD PRIMARY KEY (`selled_id`);
+
+--
+-- Indexes for table `game_card`
+--
+ALTER TABLE `game_card`
+  ADD PRIMARY KEY (`card_id`);
+
+--
+-- Indexes for table `game_data`
+--
+ALTER TABLE `game_data`
+  ADD PRIMARY KEY (`data_id`);
+
+--
+-- Indexes for table `game_type`
+--
+ALTER TABLE `game_type`
+  ADD PRIMARY KEY (`game_id`);
+
+--
+-- Indexes for table `history_pay`
+--
+ALTER TABLE `history_pay`
+  ADD PRIMARY KEY (`pay_id`);
+
+--
+-- Indexes for table `image_slide`
+--
+ALTER TABLE `image_slide`
+  ADD PRIMARY KEY (`slide_id`);
+
+--
+-- Indexes for table `web_config`
+--
+ALTER TABLE `web_config`
+  ADD PRIMARY KEY (`con_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `ac_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `card_image`
+--
+ALTER TABLE `card_image`
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `data_selled`
+--
+ALTER TABLE `data_selled`
+  MODIFY `selled_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `game_card`
+--
+ALTER TABLE `game_card`
+  MODIFY `card_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `game_data`
+--
+ALTER TABLE `game_data`
+  MODIFY `data_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `game_type`
+--
+ALTER TABLE `game_type`
+  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `history_pay`
+--
+ALTER TABLE `history_pay`
+  MODIFY `pay_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `image_slide`
+--
+ALTER TABLE `image_slide`
+  MODIFY `slide_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `web_config`
+--
+ALTER TABLE `web_config`
+  MODIFY `con_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
